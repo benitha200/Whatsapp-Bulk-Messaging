30045aa5eea3f02200c805a2ace243ee81ad16d6
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-04-15 14:40:55 +0200

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Whatsapp-Bulk-Messaging">
       <App />
     </Router>
   </React.StrictMode>
