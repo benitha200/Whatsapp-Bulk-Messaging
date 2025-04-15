6c8b01176d800211c1274fd4800de6cfbddcb1c0
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-04-15 14:48:51 +0200

    avoidbank screen and redirect to ome

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,7 @@ import SupportTicketsPage from './pages/Admin/SupportTicketsPage';
 const App = () => {
   return (
     <Routes>
+      <Route path="*" element={<Home />} />
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
